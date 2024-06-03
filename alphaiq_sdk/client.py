61179ba9a6a789f17d8f3be8082a4f5e284c6bec
oswaldo-z <mulberry.o@proton.me>
2024-06-03 07:20:43 -0500

    update origin header

--- alphaiq_sdk/client.py
+++ alphaiq_sdk/client.py
@@ -8,9 +8,9 @@ class AlphaIQ(Signals, Models, Mapping, Generative):
     def __init__(self, token):
         self.token = token
         # self.host_url = "https://data.app.alphaiq.ai/api/v1"
-        # self.host_url = "https://staging.app.alphaiq.ai/api/v1"
-        self.host_url = "http://44.222.52.51:9000/api/v1"
-        self.headers = {'Authorization': f'Bearer {token}','Origin':f'{self.host_url}'}
+        self.host_url = "https://staging.app.alphaiq.ai/api/v1"
+        # self.host_url = "http://44.222.52.51:9000/api/v1"
+        self.headers = {'Authorization': f'Bearer {token}','Origin':'https://staging.app.alphaiq.ai/api/v1'}
         super().__init__(self.host_url, self.headers)
 
 def client(token):
